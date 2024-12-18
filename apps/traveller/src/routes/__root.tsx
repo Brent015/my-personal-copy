30302f41253d60f78adc91518a7f6b062a65624c
lanzosuarez <lanzosuarez@gmail.com>
2024-12-19 03:25:22 +0800

    removed unsuded import

--- apps/traveller/src/routes/__root.tsx
+++ apps/traveller/src/routes/__root.tsx
@@ -1,6 +1,5 @@
 import { BottomBar } from "@/components/common";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
   component: () => (
